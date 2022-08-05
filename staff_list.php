20493e1e249a47ef4058419f9b222f77ae6422c8
uqo-g-nishida <g.nishida@uqo.jp>
2022-08-05 15:22:46 +0900

    Update staff

---
+++ staff_list.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>スタッフ一覧</title>
+</head>
+<body>
+
+<?php
+
+try {
+    // DB接続
+    $dsn = 'mysql:dbname=shop;host=localhost';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->query('SET NAMES utf8');
+
+    // データ追加
+    $sql = 'SELECT * FROM mst_staff WHERE 1';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+
+    $dbh = null;
+
+    echo '
+        <h1>スタッフ一覧</h1><br>
+        <form method="post" action="staff_edit.php">
+        ';
+
+    while (true) {
+        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$rec) break;
+
+        echo "
+            <input type='radio' name='staffcode' value='${rec['code']}'>
+            ${rec['code']}
+            ${rec['name']}
+            <br>
+            ";
+    }
+
+    echo '
+        <input type="submit" value="修正">
+        </form>
+        ';
+
+} catch (Exception $e) {
+    echo 'ただいま障害により大変ご迷惑をおかけしております。';
+    exit();
+}
+
+?>
+
+</body>
+</html>
