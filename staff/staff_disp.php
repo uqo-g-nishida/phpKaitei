674dd434b458ae9ea9b4348fb62fecadb759fb72
uqo-g-nishida <g.nishida@uqo.jp>
2022-08-07 14:28:24 +0900

    Update 軽い改修

--- staff/staff_disp.php
+++ staff/staff_disp.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>スタッフ編集</title>
+    <title>スタッフ修正</title>
 </head>
 <body>
 
