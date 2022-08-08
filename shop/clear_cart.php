af59526ae5e8400879b8340a4fa1e37d2b1f6a64
uqo-g-nishida <g.nishida@uqo.jp>
2022-08-08 18:48:50 +0900

    Update shop

---
+++ shop/clear_cart.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+$_SESSION = array();
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 42000, '/');
+}
+session_destroy();
+?>
+
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ログアウト</title>
+</head>
+<body>
+カートを空にしました。<br>
+</body>
+</html>
