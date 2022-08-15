09ccf88ea8dd9f141453f5734bb6c97eb2332dca
uqo-g-nishida <g.nishida@uqo.jp>
2022-08-15 13:36:32 +0900

    Update product_list

--- shop/index.php
+++ shop/index.php
@@ -1,32 +1,13 @@
 <?php
+// Twigライブラリの読込み
+require_once '../vendor/autoload.php';
+
+// Twigを使用するテンプレートの読込み
+$loader = new \Twig\Loader\FilesystemLoader('./view');
+$twig = new \Twig\Environment($loader);
+
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['member_login']) == false) {
-    echo '
-        ようこそゲスト様
-        <a href="member_login.html">会員ログイン</a><br>
-        <br>
-        ';
-} else {
-    echo "
-        ようこそ${_SESSION['member_name']}様
-        <a href='member_logout.php'>ログアウト</a><br>
-        <br>
-        ";
-}
-?>
-<!doctype html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>商品一覧</title>
-</head>
-<body>
-
-<?php
 
 try {
     // DB接続
@@ -42,23 +23,18 @@ try {
 
     $dbh = null;
 
-    echo '
-        <h1>商品一覧</h1><br>
-        ';
+    $products = array();
 
     while (true) {
         $rec = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if (!$rec) break;
 
-        echo "
-            <a href='shop_product.php?procode={$rec['code']}'>
-            ${rec['code']}
-            ${rec['name']}
-            ${rec['price']}
-            </a>
-            <br>
-            ";
+        $products[] = [
+            'code' => $rec['code'],
+            'name' => $rec['name'],
+            'price' => $rec['price']
+        ];
     }
 
 } catch (Exception $e) {
@@ -66,14 +42,13 @@ try {
     exit();
 }
 
-?>
+// htmlに渡すデータ
+$data = array(
+    'title' => '商品一覧',
+    'login' => isset($_SESSION['member_login']),
+    'member_name' => $_SESSION['member_name'],
+    'products' => $products
+);
 
-<br>
-<a href="shop_cartlook.php">カートを見る</a><br>
-<a href="clear_cart.php">カートを空にする</a><br>
-
-<br>
-<a href="../staff_login/staff_login.html">スタッフ専用画面</a>
-
-</body>
-</html>
+// テンプレートのレンダリング
+echo $twig->render('index.html.twig', $data);
