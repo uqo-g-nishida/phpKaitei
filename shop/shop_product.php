4331a387fcaf3cc3e35eee827386bf31f8d5abf9
uqo-g-nishida <g.nishida@uqo.jp>
2022-08-15 14:11:03 +0900

    Update shop_product

--- shop/shop_product.php
+++ shop/shop_product.php
@@ -1,32 +1,13 @@
 <?php
+// Twigライブラリの読込み
+require_once '../vendor/autoload.php';
+
+// Twigを使用するテンプレートの読込み
+$loader = new \Twig\Loader\FilesystemLoader('./view');
+$twig = new \Twig\Environment($loader);
+
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['member_login']) == false) {
-    echo '
-        ようこそゲスト様
-        <a href="member_login.html">会員ログイン</a><br>
-        <br>
-        ';
-} else {
-    echo "
-        ようこそ${_SESSION['member_name']}様
-        <a href='member_logout.php'>ログアウト</a><br>
-        <br>
-        ";
-}
-?>
-<!doctype html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>商品参照</title>
-</head>
-<body>
-
-<?php
 
 try {
     $pro_code = $_GET['procode'];
@@ -44,26 +25,27 @@ try {
     $stmt->execute($data);
 
     $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-    $pro_name = $rec['name'];
-    $pro_price = $rec['price'];
-    $pro_gazou_name = $rec['gazou'];
+
+    $product = array(
+        'code' => $pro_code,
+        'name' => $rec['name'],
+        'price' => $rec['price'],
+        'gazou_name' => $rec['gazou']
+    );
+
     $dbh = null;
 } catch (Exception $e) {
     echo 'ただいま障害により大変ご迷惑をおかけしております。';
     exit();
 }
 
-?>
+// htmlに渡すデータ
+$data = array(
+    'title' => '商品参照',
+    'login' => isset($_SESSION['member_login']),
+    'member_name' => $_SESSION['member_name'],
+    'product' => $product
+);
 
-<h1>商品参照</h1><br>
-商品コード：<?= $pro_code ?><br>
-商品名：<?= $pro_name ?><br>
-価格：<?= $pro_price ?>円<br>
-<img src="<?= "../product/gazou/${pro_gazou_name}" ?>"><br>
-<a href="shop_cartin.php?procode=<?= $pro_code ?>">カートに入れる</a><br>
-<form>
-    <input type="button" onclick="history.back()" value="戻る">
-</form>
-
-</body>
-</html>
+// テンプレートのレンダリング
+echo $twig->render('shop_product.html.twig', $data);
